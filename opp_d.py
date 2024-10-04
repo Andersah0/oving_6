901812dbdbf22a2fa3b8a01b294cdae5f6028cef
Sja-anat <viljar.ag@gmail.com>
2024-10-04 18:01:47 +0200

    Update opp_d.py

--- opp_d.py
+++ opp_d.py
@@ -1,3 +1,11 @@
-# Oppgave d
-
-print("Test, test")
+def datatilliste_sola(data):
+    time, temp, pres = [], [], []
+    with open(data, 'r') as f:
+        a = f.readlines()
+        for i in a[1:-1]:
+            j = i.replace(',' , '.')
+            x = j.split(';')
+            time.append(x[2])
+            temp.append(float(x[3]))
+            pres.append(float(x[4][:-1]))
+    return(time, temp, pres)
