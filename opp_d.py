ee60a9384b0543b116606ee3a3af6a78ebeecbc9
Sja-anat <viljar.ag@gmail.com>
2024-10-04 18:24:25 +0200

    Update opp_d.py

--- opp_d.py
+++ opp_d.py
@@ -1,6 +1,6 @@
-def datatilliste_sola(data):
+def datatilliste_sola():
     time, temp, pres = [], [], []
-    with open(data, 'r') as f:
+    with open('temperatur_trykk_met_samme_rune_time_datasett.csv.txt', 'r') as f:
         a = f.readlines()
         for i in a[1:-1]:
             j = i.replace(',' , '.')
@@ -9,3 +9,8 @@ def datatilliste_sola(data):
             temp.append(float(x[3]))
             pres.append(float(x[4][:-1]))
     return(time, temp, pres)
+
+
+
+
+print(datatilliste_sola())
