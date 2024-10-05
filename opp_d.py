f274662cb018d9f957c7db7ea6af1a059c65c4af
Sja-anat <viljar.ag@gmail.com>
2024-10-05 12:52:11 +0200

    Update opp_d.py

--- opp_d.py
+++ opp_d.py
@@ -7,10 +7,22 @@ def datatilliste_sola():
             x = j.split(';')
             time.append(x[2])
             temp.append(float(x[3]))
-            pres.append(float(x[4][:-1]))
+            pres.append(float(x[4]))
     return(time, temp, pres)
 
+def datatilliste_uis():
+    time, temp, pres, pres_a = [], [], [], []
+    with open('trykk_og_temperaturlogg_rune_time.csv.txt', 'r') as f:
+        a = f.readlines()
+        for i in a[1:]:
+            j = i.replace(',' , '.')
+            x = j.split(';')
+            time.append(x[0])
+            temp.append(float(x[4]))
+            pres.append(float(x[3]))
+            if x[2] == '':
+                pres_a.append('')
+            else:
+                pres_a.append(float(x[2]))
+    return(time, temp, pres, pres_a)
 
-
-
-print(datatilliste_sola())
